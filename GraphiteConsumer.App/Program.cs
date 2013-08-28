d30ec286a3f80d3925d555c682a9a67329f76335
matthew butt <matthew.butt@7digital.com>
2013-08-28 12:35:20 +0100

    Created a console app consumer. Yes I know this won't run on a Pi. It's a POC.

---
+++ GraphiteConsumer.App/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using GraphiteConsumer.Graphite;
+using GraphiteConsumer.Model;
+
+namespace GraphiteConsumer.App
+{
+	static class Program
+	{
+		static void Main(string[] args)
+		{
+			var graphiteParams = new GraphiteParams(new[] {"summarize(stats_counts.Prod.PaymentsApi.AddCard.Results.*,\"1hour\")"}, "-2d", "-");
+
+			var rawData = GraphiteDataRetriever.GetRawData(graphiteParams);
+
+			var data = Data.FromRawData(rawData);
+
+			Console.Write(data);
+
+			Console.ReadKey();
+		}
+	}
+}
---
+++ GraphiteConsumer.App/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GraphiteConsumer.App")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GraphiteConsumer.App")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("75263e41-f4ea-46b2-b259-ad35bb3f0c00")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
