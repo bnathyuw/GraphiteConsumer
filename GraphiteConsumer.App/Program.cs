e5190b90d31b04922c30ced67e4c4259b12c4ccf
matthew butt <matthew.butt@7digital.com>
2013-08-28 14:51:51 +0100

    Refactor params from console arguments.

--- GraphiteConsumer.App/Program.cs
+++ GraphiteConsumer.App/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using GraphiteConsumer.Graphite;
 using GraphiteConsumer.Model;
 
@@ -8,7 +9,9 @@ namespace GraphiteConsumer.App
 	{
 		static void Main(string[] args)
 		{
-			var graphiteParams = new GraphiteParams(new[] {"summarize(stats_counts.Prod.PaymentsApi.AddCard.Results.*,\"1hour\")"}, "-2d", "-");
+			var consoleParams = ConsoleParams.From(args);
+			
+			var graphiteParams = consoleParams.ToGraphiteParams();
 
 			var rawData = GraphiteDataRetriever.GetRawData(graphiteParams);
 
@@ -17,6 +20,46 @@ namespace GraphiteConsumer.App
 			Console.Write(data);
 
 			Console.ReadKey();
+		}
+	}
+
+	internal class ConsoleParams
+	{
+		private List<string> _targets;
+		private string _from;
+		private string _until;
+
+		public static ConsoleParams From(string[] args)
+		{
+			var targets = new List<string> ();
+			var from = "-48h";
+			var until = "-";
+			for (var i = 0; i < args.Length; i += 2)
+			{
+				var flag = args[i];
+				var value = args[i + 1];
+				switch (flag)
+				{
+					case "--target":
+					case "-t":
+						targets.Add(value);
+						break;
+					case "--from":
+					case "-f":
+						from = value;
+						break;
+					case "--until":
+					case "-u":
+						until = value;
+						break;
+				}
+			}
+			return new ConsoleParams {_targets = targets, _from = from, _until = until};
+		}
+
+		public GraphiteParams ToGraphiteParams()
+		{
+			return new GraphiteParams(_targets, _from, _until);
 		}
 	}
 }
